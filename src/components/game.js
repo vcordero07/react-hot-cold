e852fc5431f6cfcd9409320af88f1bce0a5a20c2
vcordero07 <vcordero07@gmail.com>
2018-01-15 13:33:36 -0500

    wip

--- src/components/game.js
+++ src/components/game.js
@@ -10,25 +10,45 @@ export default class game extends React.Component {
 constructor(props){
   super(props);
   this.state = {
-    on: false
+    on: false,
+    gameInfo: false
   }
 }
+
+  _displayInfo = () => {
+    this.setState({
+      gameInfo: !this.state.gameInfo
+    });
+  }
+
   render() {
     return (
       <div>
         <header>
           <nav>
             <ul>
-            <li>info</li>
-            <li>+ new game</li>
+            <li><a
+                href="#info"
+                clasName="info"
+                onClick={this._displayInfo}
+                >info
+                </a>
+            </li>
+              <li>
+              <a
+                href="#new-game"
+                clasName="new-game"
+                >+ new game
+              </a>
+            </li>
             </ul>
           </nav>
-          <h1>Hot or Cold</h1>
+          <h1 >Hot or Cold</h1>
         </header>
         <main>
           <Guess />
           <Feedback />
-          <Info />
+          <div style={{display: this.state.gameInfo ? 'block' : 'none' }}><Info  /></div>
         </main>
       </div>
     );
